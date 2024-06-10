8e9d1470fbed4402478a7f26541cfdfda513414b
Hafis20 <hafisvh2002@gmail.com>
2024-06-10 16:59:33 +0530

    Added required data validation on add user route

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -13,12 +13,19 @@ export class UserService {
     //Function for getting the all the users
     async getAllUsers() {
         try {
-            
+            const users = await this.userModel.find();
+            // If there is no users
+            if (!users.length) {
+                throw new HttpException('Users Not Found', HttpStatus.NOT_FOUND);
+            }
+            //Otherwise return the users list
+            return users;
         } catch (error) {
+            // If error is not an instance of HttpException
             if (!(error instanceof HttpException)) {
                 throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
             }
-
+            // Otherwise do this 
             throw error;
         }
     }
@@ -28,6 +35,11 @@ export class UserService {
     async addUser(obj: UserObj) {
         // Destructuring the object
         const { name, email, phoneNumber, address } = obj;
+
+        // Checking all the data is available
+        if (!name || !email || !phoneNumber || !address) {
+            throw new HttpException('Required data not found', HttpStatus.BAD_REQUEST)
+        }
         const addedUser = await this.userModel.create(obj);
         return addedUser;
     }
@@ -53,11 +65,11 @@ export class UserService {
                     new: true
                 }
             )
-
+            //If there is no one updated 
             if (!updatedUser) {
                 throw new HttpException('User not found', HttpStatus.NOT_FOUND);
             }
-
+            // Otherwise return the updatedUser
             return updatedUser;
         } catch (error) {
             // If the error is not an instance of HttpException, throw a generic 500 error
@@ -75,10 +87,12 @@ export class UserService {
             // Destructuring the userId from the body
             const { _id } = obj;
             const deletedUser = await this.userModel.findByIdAndDelete(_id);
-
+             
+            // If no on deleted
             if (!deletedUser) {
                 throw new HttpException('User not found', HttpStatus.NOT_FOUND);
             }
+            // Other wise return the deleted user data;
             return deletedUser;
         } catch (error) {
             // If the error is instance of Http exception
